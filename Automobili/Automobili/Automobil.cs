60b1ad198cb0ab6a8ca5662065a2e18055ced78c
LAPTOP-HCMGATHR\38595 <slamatej89@gmail.com>
2023-01-27 23:00:13 +0100

    Added solution for Automobili

---
+++ Automobili/Automobili/Automobil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Automobili
+{
+    public class Automobil
+    {
+        public string Naziv { get; set; }
+        public int _GodinaProizvodnje;
+        public double OsnovnaCijena { get; set; }
+
+        public int GodinaProizvodnje()
+        { return _GodinaProizvodnje; }
+
+        public void GodinaProizvodnje(int value)
+        {
+            _GodinaProizvodnje = value;
+        }
+
+        public int StarostAutomobila()
+        {
+            return DateTime.Now.Year - _GodinaProizvodnje;
+        }
+
+        public double UkupnaCijena()
+        {
+            if (OsnovnaCijena <= 70000)
+                return OsnovnaCijena * 1.3;
+            else if (OsnovnaCijena > 70000 && OsnovnaCijena < 100000)
+                return OsnovnaCijena * 1.4;
+            else
+                return OsnovnaCijena * 1.5;
+        }
+
+        public EventArgs NaPromjenuGodineProizvodnje;
+
+        public void IspisAutomobila()
+        {
+            Console.WriteLine("Naziv: " + Naziv);
+            Console.WriteLine("Godina Proizvodnje: " + GodinaProizvodnje());
+            Console.WriteLine("Starost vozila: " + StarostAutomobila());
+            Console.WriteLine("Osnovna cijena: " + OsnovnaCijena);
+            Console.WriteLine("Ukupna cijena: " + UkupnaCijena());
+        }
+    }
+
+}
+
---
+++ Automobili/Automobili/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automobili
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Automobil automobil = new Automobil();
+
+                Console.WriteLine("Unesi naziv automobila: ");
+                automobil.Naziv = Console.ReadLine();
+
+                Console.WriteLine("Unesi godinu proizvodnje automobila: ");
+                automobil.GodinaProizvodnje(int.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Unesi osnovnu cijenu automobila: ");
+                automobil.OsnovnaCijena = double.Parse(Console.ReadLine());
+
+
+                automobil.IspisAutomobila();
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Greška" + e.Message);
+            }
+        }
+    }
+    }
+

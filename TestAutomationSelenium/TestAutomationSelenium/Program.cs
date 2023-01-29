a41d36766a0b818c50547aa3853a685a4213131f
LAPTOP-HCMGATHR\38595 <slamatej89@gmail.com>
2023-01-29 12:18:10 +0100

    Added Solution

---
+++ TestAutomationSelenium/TestAutomationSelenium/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        try
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            IWebElement searchBox = driver.FindElement(By.Name("q"));
+            searchBox.SendKeys("Javatpoint tutorials");
+
+            IWebElement searchButton = driver.FindElement(By.Name("btnK"));
+            searchButton.Click();
+
+            System.Threading.Thread.Sleep(3000);
+
+            driver.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            driver.Quit();
+        }
+    }
+}
